1663ea374ee23911bd963740d859eebc5f729759
tomoki52 <konishi.tomoki.ap@alumni.tsukuba.ac.jp>
2022-08-18 22:22:26 +0900

    CLI化 limitオプション追加

--- src/wikisearch.nim
+++ src/wikisearch.nim
@@ -1,22 +1,69 @@
+const doc = """
+Usage: 
+  wikisearch <word> [--sort=[sort]] [-r | --reverse] [--limit=<limit>]
+
+Options:
+  <word>          Search word
+  --sort=<sort>   Sort by pageid/wordcount [default: pageid]
+  -r --reverse    Sort by asc
+  --limit=<limit> Limit to display [default: 10]
+"""
+
 import std/uri
 import httpclient
 import strformat
 import json
-var word = encodeUrl("ラーメン")
+import docopt
+import algorithm
+import strutils
+import sequtils
 
-var URL = fmt"http://ja.wikipedia.org/w/api.php?format=json&action=query&list=search&srsearch={word}"
+type
+  Sort = enum
+    pageid,
+    wordcount,
 
 type
   Article = object
     title: string
     snippet: string
+    pageid: int
+    wordcount: int
+type
+  SearchInfo = object
+    totalhits: int
+type
+  WikiArticle = object
+    searchinfo: SearchInfo
+    search: seq[Article]
+proc searchArticle(word: string, sort: Sort, order: SortOrder, limit: int): WikiArticle =
+  let
+    qWord: string = encodeUrl(word)
+    qSort: string = $sort
+    qLimit: int = limit
 
-var client = newHttpClient()
-let res: string = client.getContent(URL)
-let resJson: JsonNode = parseJson(res)
-echo resJson.pretty
-let 
-  resStr = to(resJson{"query","search"}, seq[Article])
+    url = fmt"http://ja.wikipedia.org/w/api.php?format=json&action=query&list=search&srsearch={qWord}&srlimit={qLimit}"
+    content = newHttpClient().getContent(url)
+  #echo parseJson(content).pretty 
+  result = to(parseJson(content){"query"}, WikiArticle)
 
-for article in resStr:
-  echo article.title
+proc main = 
+
+  let
+    args = docopt(doc, version= "0.1.0")
+    word: string = $args["<word>"]
+    sort: Sort = parseEnum[Sort]($args["--sort"])
+    order: SortOrder = if args["--reverse"]: SortOrder.Ascending else: SortOrder.Descending
+    limit: int = if args["--limit"]: parseInt($args["--limit"]) else: 10
+
+  let serchResult = searchArticle(word, sort, order, limit)
+  echo "totalhits:", serchResult.searchinfo.totalhits
+  echo"[title] pageid"
+  echo serchResult
+    .search
+    .map(proc(a: Article): string = fmt"[{a.title}] {a.pageid}")
+    .join("\n")
+
+
+
+main()
